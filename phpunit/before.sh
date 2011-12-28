019f93ba049d57897b75794fad026e4f6d1ce3f4
David Morley <dia@z8s.com>
2011-12-28 09:49:08 -0600

    give more permissions

--- phpunit/before.sh
+++ phpunit/before.sh
@@ -1,5 +1,5 @@
 sh -e /etc/init.d/xvfb start
 wget http://selenium.googlecode.com/files/selenium-server-standalone-2.15.0.jar
-java -jar selenium-server-standalone-2.15.0.jar&
+sudo java -jar selenium-server-standalone-2.15.0.jar&
 pyrus install phpunit/PHPUnit_Selenium
 phpenv rehash
