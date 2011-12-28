f035ec4aac0c7187066ebe138f5cf26f9225d182
David Morley <dia@z8s.com>
2011-12-28 10:22:21 -0600

    give it the test profile, hope the string is the same

--- phpunit/before.sh
+++ phpunit/before.sh
@@ -2,6 +2,6 @@
 sh -e /etc/init.d/xvfb start
 export DISPLAY=:99.0 && firefox -CreateProfile test
 wget http://selenium.googlecode.com/files/selenium-server-standalone-2.15.0.jar
-export DISPLAY=:99.0 && sudo java -jar selenium-server-standalone-2.15.0.jar -firefoxProfileTemplate test &
+export DISPLAY=:99.0 && sudo java -jar selenium-server-standalone-2.15.0.jar -firefoxProfileTemplate "/home/vagrant/.mozilla/firefox/t5bdy28l.test" &
 pyrus install phpunit/PHPUnit_Selenium
 phpenv rehash
