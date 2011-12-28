1ad2b07ff72e5500faa0e8fdc344fa2599447a45
David Morley <dia@z8s.com>
2011-12-28 10:27:02 -0600

    ok check it in and keep

--- phpunit/before.sh
+++ phpunit/before.sh
@@ -1,7 +1,7 @@
 
 sh -e /etc/init.d/xvfb start
-export DISPLAY=:99.0 && firefox -CreateProfile test
+#export DISPLAY=:99.0 && firefox -CreateProfile test
 wget http://selenium.googlecode.com/files/selenium-server-standalone-2.15.0.jar
-export DISPLAY=:99.0 && sudo java -jar selenium-server-standalone-2.15.0.jar -firefoxProfileTemplate "/home/vagrant/.mozilla/firefox/t5bdy28l.test" &
+export DISPLAY=:99.0 && sudo java -jar selenium-server-standalone-2.15.0.jar -firefoxProfileTemplate "./phpunit/knmqtcr7.test" &
 pyrus install phpunit/PHPUnit_Selenium
 phpenv rehash
