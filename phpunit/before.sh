e567b16768eea15dbeec7d967a939d986ce41260
David Morley <dia@z8s.com>
2011-12-28 09:51:51 -0600

    give a ff profile

--- phpunit/before.sh
+++ phpunit/before.sh
@@ -1,5 +1,5 @@
 sh -e /etc/init.d/xvfb start
 wget http://selenium.googlecode.com/files/selenium-server-standalone-2.15.0.jar
-sudo java -jar selenium-server-standalone-2.15.0.jar&
+sudo java -jar selenium-server-standalone-2.15.0.jar -firefoxProfileTemplate "./phpunit/ff.profile"&
 pyrus install phpunit/PHPUnit_Selenium
 phpenv rehash
