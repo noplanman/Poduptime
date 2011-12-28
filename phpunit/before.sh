56251797d460a4f04c5eba0430cdb7b4ac51d7e1
David Morley <dia@z8s.com>
2011-12-28 08:45:49 -0600

    crazy

--- phpunit/before.sh
+++ phpunit/before.sh
@@ -3,7 +3,7 @@ wget http://selenium.googlecode.com/files/selenium-server-standalone-2.15.0.jar
 java -jar selenium-server-standalone-2.15.0.jar&
 pyrus install phpunit/PHPUnit_Selenium
 phpenv rehash
-sh -e /etc/init.d/xvfb start
+#sh -e /etc/init.d/xvfb start
 Xvfb :99.0 -screen 0 800x600x24&
-export DISPLAY=:99.0
+#export DISPLAY=:99.0
 echo "tacocat";
