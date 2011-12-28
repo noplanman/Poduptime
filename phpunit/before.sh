0aa90459510dfd48b472cbbab1c826c33b464751
David Morley <dia@z8s.com>
2011-12-28 07:12:06 -0600

    setup selenium in phpunit

---
+++ phpunit/before.sh
@@ -0,0 +1,3 @@
+sh -e /etc/init.d/xvfb start
+export DISPLAY=:99.0
+
---
+++ phpunit/main.php
@@ -0,0 +1,19 @@
+<?php
+require_once 'PHPUnit/Extensions/SeleniumTestCase.php';
+ 
+class WebTest extends PHPUnit_Extensions_SeleniumTestCase
+{
+    protected function setUp()
+    {
+        $this->setBrowser('*firefox');
+        $this->setBrowserUrl('http://podupti.me/');
+    }
+ 
+    public function testTitle()
+    {
+        $this->open('http://podupti.me/');
+        $this->assertTitle('Diaspora Pod uptime - Find your new social home');
+    }
+}
+?>
+
