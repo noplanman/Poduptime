48b649fd19574dff9d3adf69ca3a586625fde225
David Morley <dia@z8s.com>
2011-12-28 08:29:21 -0600

    install firefox

--- phpunit/before.sh
+++ phpunit/before.sh
@@ -1,3 +1,4 @@
+sudo apt-get install firefox
 wget http://selenium.googlecode.com/files/selenium-server-standalone-2.15.0.jar
 java -jar selenium-server-standalone-2.15.0.jar&
 pyrus install phpunit/PHPUnit_Selenium
