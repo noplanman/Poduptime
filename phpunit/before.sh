70d0bcd86cddcf31f8435126bc2098b1a7030275
David Morley <dia@z8s.com>
2011-12-28 10:12:12 -0600

    have ff make a local profile

--- phpunit/before.sh
+++ phpunit/before.sh
@@ -1,4 +1,6 @@
+
 sh -e /etc/init.d/xvfb start
+firefox -CreateProfile ./phpunit/ff.profile
 wget http://selenium.googlecode.com/files/selenium-server-standalone-2.15.0.jar
 sudo java -jar selenium-server-standalone-2.15.0.jar -firefoxProfileTemplate "./phpunit/ff.profile/"&
 pyrus install phpunit/PHPUnit_Selenium
