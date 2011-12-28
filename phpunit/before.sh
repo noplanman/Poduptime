e19b456e924af13d0c7b6945a4e831d1e35a40f9
David Morley <dia@z8s.com>
2011-12-28 10:13:52 -0600

    ex display

--- phpunit/before.sh
+++ phpunit/before.sh
@@ -1,7 +1,7 @@
 
 sh -e /etc/init.d/xvfb start
-firefox -CreateProfile ./phpunit/ff.profile
+export DISPLAY=:99.0 && firefox -CreateProfile ./phpunit/ff.profile
 wget http://selenium.googlecode.com/files/selenium-server-standalone-2.15.0.jar
-sudo java -jar selenium-server-standalone-2.15.0.jar -firefoxProfileTemplate "./phpunit/ff.profile/"&
+export DISPLAY=:99.0 && sudo java -jar selenium-server-standalone-2.15.0.jar -firefoxProfileTemplate "./phpunit/ff.profile/"&
 pyrus install phpunit/PHPUnit_Selenium
 phpenv rehash
