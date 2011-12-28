5870ab44e039fdbedfc50b3962a170640300f64c
David Morley <dia@z8s.com>
2011-12-28 09:18:44 -0600

    try sel last

--- phpunit/before.sh
+++ phpunit/before.sh
@@ -1,9 +1,5 @@
-#sudo apt-get install firefox
+sh -e /etc/init.d/xvfb start
 wget http://selenium.googlecode.com/files/selenium-server-standalone-2.15.0.jar
 java -jar selenium-server-standalone-2.15.0.jar&
 pyrus install phpunit/PHPUnit_Selenium
 phpenv rehash
-sh -e /etc/init.d/xvfb start
-#Xvfb :99.0 -screen 0 800x600x24&
-export DISPLAY=:99.0
-echo "tacocat";
