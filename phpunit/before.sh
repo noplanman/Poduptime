9f0b468261e647938a1f62bcb97b853008018c78
David Morley <dia@z8s.com>
2011-12-28 08:43:41 -0600

    no ff, see if need to start xv

--- phpunit/before.sh
+++ phpunit/before.sh
@@ -1,8 +1,9 @@
-sudo apt-get install firefox
+#sudo apt-get install firefox
 wget http://selenium.googlecode.com/files/selenium-server-standalone-2.15.0.jar
 java -jar selenium-server-standalone-2.15.0.jar&
 pyrus install phpunit/PHPUnit_Selenium
 phpenv rehash
 sh -e /etc/init.d/xvfb start
+Xvfb :99.0 -screen 0 800x600x24&
 export DISPLAY=:99.0
 echo "tacocat";
