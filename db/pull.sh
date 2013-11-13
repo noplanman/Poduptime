ef256991281b5608c4e2be7050ca528a70b598e0
David <david@usr.io>
2013-11-13 06:52:24 -0800

    add and label config

--- db/pull.sh
+++ db/pull.sh
@@ -1,20 +1,24 @@
 WGET="/usr/bin/wget"
 if [ -s /tmp/index.google ];then
-        echo "already running"
+        echo "already running die"
 exit;
 else
-echo "Checking for internet";
+	echo "Checking for internet";
 fi
 
-$WGET -q --tries=10 --timeout=15 http://www.google.com -O /tmp/index.google &> /dev/null
+$WGET -d -v --tries=10 --timeout=15 http://www.google.com -O /tmp/index.google
+# &> /dev/null
+sleep 2
+
 if [ ! -s /tmp/index.google ];then
 	echo "could not update pods as no internet"
-rm /tmp/index.google
+	rm /tmp/index.google
+exit;
 else
-echo "Pulling in new pod data";
-cd /var/www/podup/db
-php5 pull.php
-touch last.data
-php5 backup.php
-rm /tmp/index.google
+	echo "Pulling in new pod data";
+	cd /var/www/podup/db
+	php5 pull.php
+	touch last.data
+	php5 backup.php
+	rm /tmp/index.google
 fi
