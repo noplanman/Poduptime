26af3af4e4d7ee8bc9d183db775190a07992321a
MatrixCrawler <matrixcrawler@chaosdimension.de>
2014-03-10 20:16:01 +0100

    Introduced PDO-SQL and new Config

--- db/saverating.php
+++ db/saverating.php
@@ -12,35 +12,37 @@ if (!$_POST['userurl']){
 }
 
 if (!$_POST['domain']){
-  echo "no pod domain given";
- die;
+    echo "no pod domain given";
+    die;
 }
 if (!$_POST['comment']){
-  echo "no comment";
- die;
+    echo "no comment";
+    die;
 }
 if (!$_POST['rating']){
-  echo "no rating given";
- die;
+    echo "no rating given";
+    die;
 }
 
- $dbh = pg_connect("dbname=$pgdb user=$pguser password=$pgpass");
-     if (!$dbh) {
-         die("Error in connection: " . pg_last_error());
-     }
-     $sql = "INSERT INTO rating_comments (domain, comment, rating, username, userurl) VALUES($1, $2, $3, $4, $5)";
-     $result = pg_query_params($dbh, $sql, array($_POST['domain'], $_POST['comment'], $_POST['rating'], $_POST['username'], $_POST['userurl']));
-     if (!$result) {
-         die("Error in SQL query: " . pg_last_error());
-     }
-     $to = $adminemail;
-     $subject = "New rating added to poduptime ";
-     $message = "Pod:" . $_POST["domain"] . "\n\n";
-     $headers = "From: ".$_POST["email"]."\r\n";
-     @mail( $to, $subject, $message, $headers );    
+$dbConnection = DB::connectDB();
 
-     echo "Comment posted!";
-     pg_free_result($result);
-     pg_close($dbh);
+if (!$dbh) {
+    die("Error in connection: " . $dbConnection->errorInfo()[2]);
+}
+
+$sql = "INSERT INTO rating_comments (domain, comment, rating, username, userurl)"
+    . " VALUES(".$dbConnection->quote($_POST['domain']).", ".$dbConnection->quote($_POST['comment']).", ".$dbConnection->quote($_POST['rating']). ","
+    . " ".$dbConnection->quote($_POST['username']).", ".$dbConnection->quote($_POST['userurl']).")";
+if (!$result) {
+    die("Error in SQL query: " . pg_last_error());
+}
+
+
+$subject = "New rating added to poduptime ";
+$message = "Pod:" . $_POST["domain"] . "\n\n";
+$headers = "From: ".$_POST["email"]."\r\n";
+@mail( ADMIN_EMAIL, $subject, $message, $headers );
+
+echo "Comment posted!";
 
 ?>
