b029b2d871500231a842a13be5904fb5d4be7074
dmorley <root@usr.io>
2016-01-20 10:31:00 -0800

    add some basic logging to add.php

--- db/add.php
+++ db/add.php
@@ -2,27 +2,29 @@
 <?php
 $valid=0;
  include('config.php');
+ include('../logging.php');
+$log = new Logging();
+$log->lfile($log_dir."/add.php.log");
 if (!$_POST['url']){
-  echo "no url given";
+  echo "no url given";$log->lwrite('no url given '.$_POST['domain']);
  die;
 }
 if (!$_POST['email']){
-  echo "no email given";
+  echo "no email given";$log->lwrite('no email given '.$_POST['domain']);
  die;
 }
 if (!$_POST['domain']){
-  echo "no pod domain given";
+  echo "no pod domain given";$log->lwrite('no domain given '.$_POST['domain']);
  die;
 }
 if (!$_POST['url']){
-  echo "no API key for your stats";
+  echo "no API key for your stats";$log->lwrite('no api given '.$_POST['domain']);
  die;
 }
 if (strlen($_POST['url']) < 14){
-  echo "API key bad needs to be like m58978-80abdb799f6ccf15e3e3787ee";
+  echo "API key bad needs to be like m58978-80abdb799f6ccf15e3e3787ee";$log->lwrite('api key too short '.$_POST['domain']);
  die;
 }
-
  $dbh = pg_connect("dbname=$pgdb user=$pguser password=$pgpass");
      if (!$dbh) {
          die("Error in connection: " . pg_last_error());
@@ -34,10 +36,10 @@ if (strlen($_POST['url']) < 14){
  }
  while ($row = pg_fetch_array($result)) {
 if ($row["domain"] == $_POST['domain']) {
-echo "domain already exists";die;
+echo "domain already exists";$log->lwrite('domain already exists '.$_POST['domain']);die;
 }
 if ($row["pingdomurl"] == $_POST['url']) {
-echo "API key already exists";die;
+echo "API key already exists";$log->lwrite('API key already exists '.$_POST['domain']);die;
 }
  }
 
@@ -64,16 +66,16 @@ echo "API key already exists";die;
         curl_close($ch);
 
 if (stristr($outputssl, 'nodeName')) {
-  echo "Your pod has ssl and is valid<br>";
+  echo "Your pod has ssl and is valid<br>";$log->lwrite('Your pod has ssl and is valid '.$_POST['domain']);
   $valid=1;
 }
 if (stristr($output, 'nodeName')) {
-  echo "Your pod does not have ssl but is a valid pod<br>";
+  echo "Your pod does not have ssl but is a valid pod<br>";$log->lwrite('Your pod does not have ssl but is a valid pod '.$_POST['domain']);
   $valid=1;
 }
 
 if ($valid=="1") {    
-     $sql = "INSERT INTO pods (domain, pingdomurl, email) VALUES($1, $2, $3)";
+     $sql = "INSERT INTO pods (domain, pingdomurl, email) VALUES($1, $2, $3)";$log->lwrite($sql);
      $result = pg_query_params($dbh, $sql, array($_POST['domain'], $_POST['url'], $_POST['email']));
      if (!$result) {
          die("Error in SQL query: " . pg_last_error());
@@ -92,7 +94,7 @@ if ($valid=="1") {
     
      pg_close($dbh);
 } else {
-echo "Could not validate your pod on http or https, check your setup!";
+echo "Could not validate your pod on http or https, check your setup!<br>Take a look at <a href='https://".$_POST['domain']."/nodeinfo/1.0'>your /nodeinfo</a>";$log->lwrite('Could not validate your pod on http or https, check your setup! '.$_POST['domain']);
 }
-
+$log->lclose();
 ?>
