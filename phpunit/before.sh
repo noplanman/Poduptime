d0688b90c84dd23894b33d11d7732d13f16c1366
David Morley <support@diasp.org>
2016-12-21 07:04:50 -0500

    remove tests, been there years and never used  or setup

--- phpunit/before.sh
+++
@@ -1,8 +0,0 @@
-
-sh -e /etc/init.d/xvfb start
-#export DISPLAY=:99.0 && firefox -CreateProfile test
-#wget http://selenium.googlecode.com/files/selenium-server-standalone-2.15.0.jar
-#export DISPLAY=:99.0 && sudo java -jar selenium-server-standalone-2.15.0.jar -firefoxProfileTemplate "./phpunit/knmqtcr7.test" &
-#pyrus install phpunit/PHPUnit_Selenium
-composer install
-phpenv rehash
