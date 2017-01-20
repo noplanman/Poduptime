7f7f8db8c61592ff00ca01f94bda2037aab05add
dmorley <support@diasp.org>
2017-01-20 06:02:01 -0500

    deal with cname and try less flaky dns

--- db/pull.php
+++ db/pull.php
@@ -154,13 +154,14 @@ while ($row = pg_fetch_assoc($result)) {
   }
 
   _debug('Signup Open', $signup);
-  $ip6 = exec(escapeshellcmd('dig @4.2.2.2 +nocmd ' . $domain . ' aaaa +noall +short'));
+  $ip6 = exec(escapeshellcmd('dig @74.82.42.42 +nocmd ' . $domain . ' aaaa +noall +short'));
   $iplookup = [];
-  exec(escapeshellcmd('delv @4.2.2.2 ' . $domain), $iplookup);
+  exec(escapeshellcmd('delv @74.82.42.42 ' . $domain), $iplookup);
   if ($iplookup) {
     _debug('Iplookup', $iplookup, true);
     $dnssec = in_array('; fully validated', $iplookup) ?? false ;
-    preg_match('/A(.*)/', $iplookup[1], $version);
+    $getaonly = array_values(preg_grep('/A\s.*/', $iplookup));
+    preg_match('/A\s(.*)/', $getaonly[0], $version);
     $ip   = trim($version[1]);
   }
   $ip || $score -= 2;
