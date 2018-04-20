dd98a582def5a441ca9d29182e5a9065243a1517
dmorley <david@usr.io>
2018-04-20 10:02:46 -0700

    safe words

--- podcrawler.php
+++ podcrawler.php
@@ -1,6 +1,8 @@
 <?php
 
 $json = json_decode(file_get_contents('https://the-federation.info/pods.json'));
-foreach($json->pods as $poddata) {
-  echo exec("php-cgi db/add.php domain={$poddata->host}") . "\r\n";
+if ($json) {
+  foreach ($json->pods ?? [] as $poddata) {
+    echo exec("php-cgi db/add.php domain={$poddata->host}") . "\r\n";
+  }
 }
