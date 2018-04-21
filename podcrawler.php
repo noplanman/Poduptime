113fa0b4599873df6119b188f067f97b8e6d3e4b
dmorley <david@usr.io>
2018-04-20 19:13:58 -0700

    stop killing prod from robots

--- podcrawler.php
+++ podcrawler.php
@@ -1,8 +1,12 @@
 <?php
 
-$json = json_decode(file_get_contents('https://the-federation.info/pods.json'));
-if ($json) {
-  foreach ($json->pods ?? [] as $poddata) {
-    echo exec("php-cgi db/add.php domain={$poddata->host}") . "\r\n";
+if (php_sapi_name() == "cli") {
+  $json = json_decode(file_get_contents('https://the-federation.info/pods.json'));
+  if ($json) {
+    foreach ($json->pods ?? [] as $poddata) {
+      echo exec("php-cgi db/add.php domain={$poddata->host}") . "\r\n";
+    }
   }
+} else {
+header('HTTP/1.0 403 Forbidden');
 }
